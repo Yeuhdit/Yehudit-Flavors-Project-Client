9403e4818c7cebbd89858a3723b0b1d5c7f9e4ef
yeudit <y00556735623@gmail.com>
2025-12-19 09:39:23 +0200

    Create Redux store

---
+++ src/features/common/store.js
@@ -0,0 +1,8 @@
+// להקים חנות Redux שתכלול תוכניות לאחסון הנתונים שלך, כמו גם את כל המידעים הנדרשים על ידי reducers, middleware, ועוד.
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    // נוסיף slices אחר כך
+  }
+});
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './features/common/store.js' // ניצור בקרוב
+import { store } from './features/common/store.js' 
 import { HashRouter as Router } from 'react-router-dom'
-import App from './features/common/App.jsx'
+// import App from './features/common/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
