8c4a30b0aa4fd2825db2415de1ede7a30f8e1786
yeudit <y00556735623@gmail.com>
2025-12-19 11:32:18 +0200

    Add basic Navbar

---
+++ src/features/common/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav">
+        <Link className='nav-link' to="/">דף הבית</Link>
+        <Link className='nav-link' to="/recipes">כל המתכונים</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
