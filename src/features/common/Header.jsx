4784fec32614b87517a34961d767463032e1c5c9
yeudit <y00556735623@gmail.com>
2025-12-25 15:56:40 +0200

    Add filters to Header

--- src/features/common/Header.jsx
+++ src/features/common/Header.jsx
@@ -1,14 +1,30 @@
-import { TextField, IconButton } from '@mui/material';
+// src/features/common/Header.jsx
+import { TextField, IconButton, Chip } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+
+const difficulties = ['קל', 'בינוני', 'קשה'];
 
 const Header = () => {
   return (
     <div className="homeText">
+      {/* שדה חיפוש */}
       <TextField
         fullWidth
         placeholder="מה תרצו להכין היום?"
         InputProps={{ endAdornment: <IconButton><SearchIcon /></IconButton> }}
       />
+      
+      {/* קטגוריות */}
+      <div className="categories flex-center">
+        <Chip label="כללי" clickable />
+      </div>
+      
+      {/* רמות קושי */}
+      <div className="difficulty flex-center">
+        {difficulties.map(dif => (
+          <Chip key={dif} label={dif} clickable />
+        ))}
+      </div>
     </div>
   );
 }
