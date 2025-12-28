ef0836e8b55b95d4b5c71ab074586653e8bf6458
yeudit <y00556735623@gmail.com>
2025-12-28 16:42:05 +0200

    Add recipe pictures All recipes

--- src/features/recipes/Recipes.jsx
+++ src/features/recipes/Recipes.jsx
@@ -1,18 +1,53 @@
-//src/recipes/Recipes.jsx
-import { useSelector } from 'react-redux'
-import SingleRecipe from './SingleRecipe'
-import './Recipes.css'
+// //src/recipes/Recipes.jsx
+// import { useSelector } from 'react-redux'
+// import SingleRecipe from './SingleRecipe'
+// import './Recipes.css'
 
-const Recipes = () => {
-  const recipesList = useSelector(state => state.recipes.allRecipes)
+// const Recipes = () => {
+//   const recipesList = useSelector(state => state.recipes.allRecipes)
 
+//   return (
+//     <div className="recipes">
+//       {recipesList.map(item => (
+//         <SingleRecipe key={item._id} recipe={item} />
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default Recipes
+
+// import { mockRecipes } from './recipesData.Js';
+
+// const recipesList = mockRecipes;
+
+// export default function Recipes() {
+//   return (
+//     <div>
+//       {recipesList.map(recipe => (
+//         <img
+//           key={recipe._id}
+//           src={recipe.imageUrl}
+//           alt={recipe.name}
+//           width="200"
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+ import { mockRecipes } from './recipesData.Js';
+const recipesList = mockRecipes;
+
+export default function Recipes() {
   return (
-    <div className="recipes">
-      {recipesList.map(item => (
-        <SingleRecipe key={item._id} recipe={item} />
+    <div>
+      {mockRecipes.map(recipe => (
+        <div key={recipe._id}>
+          <img src={recipe.imageUrl} alt={recipe.name} width="200" />
+          <p>{recipe.name}</p>
+        </div>
       ))}
     </div>
-  )
+  );
 }
-
-export default Recipes
